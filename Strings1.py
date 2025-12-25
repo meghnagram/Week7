e30a11fa3059708d72d30f46c62b7f8c4e94dd14
meghnagram <meghna.mona@gmail.com>
2025-12-25 19:51:00 +0530

    Create Strings1.py

---
+++ Strings1.py
@@ -0,0 +1,14 @@
+def is_odd_indices_alpha_and_even_indices_digits(string: str) -> bool:
+    '''
+    Given a string, check if all the odd indices are alphabets and the even indices are digits.
+
+    Note: indices starts from 0.
+
+    Arguments:
+    string: str - the input string
+
+    Return:
+    bool - True if all odd indices are alphabets and even indices are digits, else False
+    '''
+    
+    return string[::2].isdigit() and string[1::2].isalpha()
