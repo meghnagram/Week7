49b6f4ee44778d68dbba6434998017b489b47ffb
meghnagram <meghna.mona@gmail.com>
2025-12-25 19:48:02 +0530

    Create Numbers1.py

---
+++ Numbers1.py
@@ -0,0 +1,15 @@
+def seconds_to_minute_seconds(seconds: int) -> tuple:
+    '''
+    Given an integer representing seconds, return a tuple of (minutes, seconds).
+
+    Arguments:
+    seconds: int - an integer representing the number of seconds.
+
+    Return: tuple - a tuple of (minutes, remaining_seconds).
+    '''
+    
+    
+    minutes = seconds // 60
+    remaining_seconds = seconds % 60
+    return (minutes, remaining_seconds)
+    
