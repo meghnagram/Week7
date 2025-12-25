8c77dadafd44119f61a3493880d5674e4e513af4
meghnagram <meghna.mona@gmail.com>
2025-12-25 19:53:04 +0530

    Create Strings4.py

---
+++ Strings4.py
@@ -0,0 +1,13 @@
+def remove_edges(s: str) -> str:
+    '''
+    Return a new string with the first two and last two 
+    characters removed from the given string. 
+
+    Arguments:
+    s: str - a string.
+
+    Return: str - a string with first and last two characters removed.
+    '''
+    
+    
+    return s[2:-2]
